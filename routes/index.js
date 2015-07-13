967235dd686010804db006ab5ec08cfe20b71460
henricoptero <henricoptero@icloud.com>
2015-07-13 18:14:46 +0200

    Creditos añadidos

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+// GET Página autor
+router.get('/author', function(req, res){
+  res.render('author');
+});
+
 module.exports = router;
